c108280192a15287d7f511e9f91b2c8cf901ae88
Markus Schwarzinger <m.schwarzinger@up-code.dev>
2026-04-22 21:31:33 +0200

    fix: update encryption key generation to use virtual environment's Python directly

--- install.sh
+++ install.sh
@@ -201,8 +201,7 @@ ok
 step "Generate encryption key"
 {
   cd "$APP_DIR/upservx-service" &&
-  source venv/bin/activate &&
-  python3 -c "from encryption import EncryptionManager; EncryptionManager.ensure_key_exists(); print('Encryption key generated')"
+  venv/bin/python3 -c "from encryption import EncryptionManager; EncryptionManager.ensure_key_exists(); print('Encryption key generated')"
 } &>/tmp/install.log &
 spin $!
 if [ $? -eq 0 ]; then ok; else fail; fi
